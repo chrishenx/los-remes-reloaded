1dd3a53604023c389a8b21022e94c657ddad71be
Christian Leon <christian.leon@sesolabor.com>
2023-10-21 19:31:29 -0600

    Antd integration with nextjs

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,25 +1,26 @@
 import { theme } from '@/lib/themeConfig';
-import { ConfigProvider, Layout, Menu } from 'antd';
+import { ConfigProvider, Layout, Menu, MenuTheme, theme as antdTheme } from 'antd';
 import Sider from 'antd/es/layout/Sider';
 import Image from 'next/image';
+import { useState } from 'react';
 
-export default function Home() {
+export function AppLayout() {
+  const [themeMode, setThemeMode] = useState<MenuTheme>('light');
+
   return (
-    <ConfigProvider theme={theme}>
+    <ConfigProvider theme={{...theme, algorithm: themeMode === 'light' ? antdTheme.defaultAlgorithm : antdTheme.darkAlgorithm}}>
       <Layout>
         <Sider trigger={null} collapsible collapsed={true}>
           <div className="logo">
             <Image src="/logo3.jpeg" alt="LosRemes Reloaded" height={100} />
           </div>
           <Menu 
-            theme="dark" 
+            theme={themeMode}
             mode="inline"
             items={[
-              {
-
-              }
-
-
+              
+            ]}
+            />
         </Sider>
       </Layout>
     </ConfigProvider>
---
+++ src/lib/themeConfig.ts
@@ -0,0 +1,9 @@
+import type { ThemeConfig } from 'antd';
+
+export const theme: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorPrimary: '#948073',
+    colorInfo: '#948073',
+  },
+};
