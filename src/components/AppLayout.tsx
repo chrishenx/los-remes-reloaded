0a039d74627db33a3903b5417c44667df56c8999
Christian Leon <christian.leon@sesolabor.com>
2023-11-01 23:53:13 -0600

    AppLayout adjustment

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -7,7 +7,8 @@ import {
   MenuUnfoldOutlined,
 } from '@ant-design/icons';
 import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
-import { PageProps } from '@/pages/pageProps';
+import { PageProps } from '@/types/pageProps';
+import Link from 'next/link';
 
 type AppLayoutProps = {
   themeMode: MenuTheme;
@@ -17,31 +18,31 @@ type AppLayoutProps = {
 export function AppLayout({ Component, pageProps, themeMode, setThemeMode }: AppLayoutProps) {
   const [collapsed, setCollapsed] = useState(true);
   const {
-    token: { colorBgContainer, colorPrimaryBg, colorText, fontSizeHeading5 },
+    token: { colorBgContainer, colorPrimaryBg, colorText },
   } = theme.useToken();
 
   return (
     <Layout style={{height: "100vh"}}>
       <Layout.Sider trigger={null} collapsedWidth={0} collapsible collapsed={collapsed} style={{background: colorPrimaryBg}} width={320}>
         <Flex justify="space-between" align="center" style={{paddingRight: 8}}>
-          <Image src="/logo3.jpeg" alt="LosRemes Reloaded" height={80} width={80} />
+          <Link href="/">
+            <Image src="/logo3.jpeg" alt="LosRemes Reloaded" height={80} width={80} />
+          </Link>
           <Typography.Title level={4} style={{margin: 0}}>
             Los Remes Reloaded
           </Typography.Title>
         </Flex>
-        <Menu theme={themeMode} mode="inline" >
-          <Menu.Item key="1" onClick={() => setThemeMode(themeMode === "light" ? "dark" : "light")}>
-            <Flex justify="space-between" align="center">
-              <Typography.Text style={{ color: colorText, fontSize: fontSizeHeading5 }}>
-                {
-                  themeMode === "light" ? "Cambiar a modo oscuro" : "Cambiar a modo claro"
-                }
-              </Typography.Text>
-              {
-                themeMode === "light" ? <BsFillSunFill /> : <BsFillMoonStarsFill />
-              }
-            </Flex>
-          </Menu.Item>
+        <Menu theme={themeMode} mode="inline" items={
+          [
+            {
+              key: "1",
+              title: themeMode === "light" ? "Cambiar a modo oscuro" : "Cambiar a modo claro",
+              label: themeMode === "light" ? "Cambiar a modo oscuro" : "Cambiar a modo claro",
+              onClick: () => setThemeMode(themeMode === "light" ? "dark" : "light"),
+              icon: themeMode === "light" ? <BsFillMoonStarsFill /> : <BsFillSunFill />,
+            },
+          ]
+        }>
         </Menu>
       </Layout.Sider>
       <Layout>
@@ -56,7 +57,7 @@ export function AppLayout({ Component, pageProps, themeMode, setThemeMode }: App
               height: 64,
             }}
           />
-          <Typography.Title level={3} style={{ display: "inline", color: colorText, margin: 0 }}>
+          <Typography.Title level={3} style={{ display: "inline", color: colorText, margin: 0, marginLeft: 4 }}>
             {pageProps.name}
           </Typography.Title>
           </Layout.Header> 
