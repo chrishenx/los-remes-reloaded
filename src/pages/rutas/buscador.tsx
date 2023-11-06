07f1bb9deb1b09bb70547e92a3677efb91e354a6
Christian Leon <christian.leon@sesolabor.com>
2023-11-05 19:20:58 -0600

    Refactor RoutesFinderPage getServerSideProps

--- src/pages/rutas/buscador.tsx
+++ src/pages/rutas/buscador.tsx
@@ -1,7 +1,7 @@
 import { RoutesFinder, RoutesFinderProps, } from '@/components/RoutesFinder';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { PageProps } from '@/types/pageProps';
-import { parseGrade } from '@/lib/climbing-utils';
+import { parseRoutesSearchParams } from '@/lib/climbing-utils';
 import sectors from "@/lib/los-remes.json";
 
 export function RoutesFinderPage({ sectors, gradeRange } : InferGetServerSidePropsType<typeof getServerSideProps>) {
@@ -14,25 +14,15 @@ export default RoutesFinderPage;
 
 export const getServerSideProps = (async (context) => {
   // TODO Validate types or query params
-  const sectorIds = new Set(context.query.sector_ids as string[]);
-
-  const rawMinGrade = context.query.min_grade;
-  const rawMaxGrade = context.query.max_grade;
-
-  const minGrade = parseGrade(rawMinGrade as string);
-  const maxGrade = parseGrade(rawMaxGrade as string);
-
-  const pageName = minGrade === maxGrade ? `Rutas de ${rawMinGrade}` : `Rutas entre ${rawMinGrade} y ${rawMaxGrade}`;
+  const { sectors: searchedSectors, gradeRange } = parseRoutesSearchParams(context.query);
+  
+  const pageName = gradeRange?.min.raw === gradeRange?.max.raw ? `Rutas de ${gradeRange?.min.raw}` : `Rutas entre ${gradeRange?.min.raw} y ${gradeRange?.max.raw}`;
   
   return {
     props: {
       name: pageName,
-      sectors: sectorIds.size === 0 ? sectors : sectors.filter(sector => sectorIds.has(sector.id)),
-      gradeRange: {
-        raw: `${rawMinGrade} a ${rawMaxGrade}`,
-        min: minGrade,
-        max: maxGrade
-      }
+      sectors: searchedSectors.length === 0 ? sectors : searchedSectors,
+      gradeRange,
     },
   };
 }) satisfies GetServerSideProps<PageProps & RoutesFinderProps>;
